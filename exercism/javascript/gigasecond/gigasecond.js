a209cb4242739a3d01b9de346c27a03be9d5ae0d
Micheal Moery <mdmoery@yahoo.com>
2017-03-28 01:59:21 -0500

    Solved gigasecond after looking at someone else's code.  I am so ashamed.

--- exercism/javascript/gigasecond/gigasecond.js
+++ exercism/javascript/gigasecond/gigasecond.js
@@ -4,9 +4,14 @@ var Gigasecond = function(input) {
 
 
 Gigasecond.prototype.date = function() {
+    return new Date(this.original.getTime() + Math.pow(10,12));
+    /* This is my orignal incomplete code.  Arsenicro's solution above.
+     * 
     var test = this.original;
     test.setSeconds(test.getSeconds() + Math.pow(10,9));
     return test;
+    
+    */
 };
 
 module.exports = Gigasecond;
