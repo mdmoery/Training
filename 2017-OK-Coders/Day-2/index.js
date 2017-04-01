7b67891eb71d942eed47b9ced0d6032215043618
Micheal Moery <mdmoery@yahoo.com>
2017-04-01 10:28:39 -0500

    Exercises up to first break

---
+++ 2017-OK-Coders/Day-2/index.js
@@ -0,0 +1,14 @@
+var name = "Princess Brunhilda von Nay Nay III";
+var age = 8; //Setting age to 8
+
+console.log("My name is " + name + " and I am age " + age);
+
+// Need to find out drinking age
+var drinkingAge = 21 - age;
+
+
+if (age < 21) {
+    console.log("...and I can have beer in " + drinkingAge + " years");
+} else {
+    console.log("leave me alone I am drinking");
+};
