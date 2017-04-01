bf0003a2f329febbd3d79c8d5dd9fbd42b208ee7
Micheal Moery <mdmoery@yahoo.com>
2017-04-01 11:09:27 -0500

    Loops

--- 2017-OK-Coders/Day-2/index.js
+++ 2017-OK-Coders/Day-2/index.js
@@ -1,14 +1,26 @@
 var name = "Princess Brunhilda von Nay Nay III";
 var age = 8; //Setting age to 8
+var tmpAge;
+var drinkingAge;
 
 console.log("My name is " + name + " and I am age " + age);
 
-// Need to find out drinking age
-var drinkingAge = 21 - age;
+
+var counter =1;
+
+while(age <= (21 - counter)){
+    tmpAge = age + counter;
+    console.log("In " +counter + " years I will be " + tmpAge);
+    counter++;
+    
+    drinkingAge = 21 - tmpAge;
 
 
-if (age < 21) {
-    console.log("...and I can have beer in " + drinkingAge + " years");
-} else {
-    console.log("leave me alone I am drinking");
+    if (tmpAge < 21) {
+        console.log("...and I can have beer in " + drinkingAge + " years");
+    } else if (tmpAge === 21) {
+        console.log ("yeah ... celebrate");
+    } else {
+        console.log("leave me alone I am drinking");
+    }
 };
