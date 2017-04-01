a5e7c27eab45db89a8be0b14e12cebe7ebc65a7e
Micheal Moery <mdmoery@yahoo.com>
2017-04-01 12:42:25 -0500

    Functions

---
+++ 2017-OK-Coders/Day-2/example.js
@@ -0,0 +1,11 @@
+var howMany = "First";
+
+
+function myFirstFunction(num){
+        console.log("This is my " + num +" function");
+        return num;
+};
+
+myFirstFunction("First");
+var result = myFirstFunction(howMany);
+console.log(result);
