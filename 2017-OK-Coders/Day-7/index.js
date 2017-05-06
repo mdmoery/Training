aa3e757651849ecaa70b065c5ac3df85471b0522
Micheal Moery <mdmoery@yahoo.com>
2017-05-06 09:18:56 -0500

    Week 7

---
+++ 2017-OK-Coders/Day-7/index.js
@@ -0,0 +1,29 @@
+var restify = require('restify');
+var server = restify.createServer();
+server.use(restify.bodyParser());
+const port = 8088;
+
+var auth = require('./routes/auth');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/testing');
+var db = mongoose.connection;
+
+db.on('error', function(msg){
+	console.log('Mongoose connection error %s', msg);
+});
+
+db.once('open', function(){
+	console.log('Mongoose connection established');
+});
+
+server.post('/user/add', auth.create);
+server.post('/user/login', auth.read);
+server.get('/', restify.serveStatic({
+	directory: './client', 
+	default: "index.html"
+}));
+
+server.listen(port, function(){
+	console.log("%s listening on %s", server.name, port);
+});
